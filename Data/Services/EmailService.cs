5b3f9458e77aaa2f5491d72792287bf158d4ce72
Jr_Silva_Dev <jos.pd2019@gmail.com>
2024-05-06 13:10:34 -0300

    FEAT: Adding Enviroment Variables/Setting host IP

--- Data/Services/EmailService.cs
+++ Data/Services/EmailService.cs
@@ -7,23 +7,23 @@ namespace EmailSender.API.Data.Services
 {
     public class EmailService : IEmailService
     {
-        //private readonly string API_KEY = Environment.GetEnvironmentVariable("API_KEY");
+        private readonly string API_KEY = Environment.GetEnvironmentVariable("SEND_EMAIL_API_KEY");
         //private readonly string DOMAIN = Environment.GetEnvironmentVariable("DOMAIN");
         //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
 
         public async Task<Response> SendEmail(EmailEntity email)
         {
-
-            var apiKey = "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";
-            var client = new SendGridClient(apiKey);
+            var client = new SendGridClient(API_KEY);
             var from = new EmailAddress(email.SenderEmailAddress, email.SenderName);
             var subject = email.EmailSubject;
             var to = new EmailAddress(email.ReceiverEmailAddress, email.ReceiverName);
-            var plainTextContent = email.EmailContent;
-            var htmlContent = $"<strong>{email.EmailContent}</strong>";
+            var plainTextContent = email.EmailSubject;
+            var htmlContent = email.EmailContent;
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
             return response;
         }
     }
 }
+
+
--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
-#See https://aka.ms/customizecontainer to learn how to customize your debug container and how Visual Studio uses this Dockerfile to build your images for faster debugging.
-
 FROM mcr.microsoft.com/dotnet/aspnet:7.0 AS base
 WORKDIR /app
 EXPOSE 80
@@ -7,14 +5,14 @@ EXPOSE 443
 
 FROM mcr.microsoft.com/dotnet/sdk:7.0 AS build
 WORKDIR /src
-COPY ["EmailSender.API.csproj", "."]
-RUN dotnet restore "./EmailSender.API.csproj"
+COPY ["EmailSender.API.csproj", "./"]
+RUN dotnet restore "EmailSender.API.csproj"
 COPY . .
-WORKDIR "/src/."
+WORKDIR "/src/"
 RUN dotnet build "EmailSender.API.csproj" -c Release -o /app/build
 
 FROM build AS publish
-RUN dotnet publish "EmailSender.API.csproj" -c Release -o /app/publish /p:UseAppHost=false
+RUN dotnet publish "EmailSender.API.csproj" -c Release -o /app/publish
 
 FROM base AS final
 WORKDIR /app
